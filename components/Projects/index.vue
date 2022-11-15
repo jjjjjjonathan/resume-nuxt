9b89b525eabd5df47ab55c1c51d8b97a4fafd09f
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-11-14 23:15:18 -0500

    Made subheadings bolder in each section

--- components/Projects/index.vue
+++ components/Projects/index.vue
@@ -2,15 +2,15 @@
   <div v-for="project in projects" class="p-2">
 
     <div class="flex flex-row justify-between items-center">
-      <h4 class="text-md font-medium"><a :href="project.url">{{ project.name }}</a>
+      <h4 class="text-md font-semibold"><a :href="project.url">{{ project.name }}</a>
       </h4>
-      <span class="font-medium text-sm">{{project.startDate}} - {{project.endDate}}</span>
+      <span class="font-medium text-sm">{{ project.startDate }} - {{ project.endDate }}</span>
     </div>
 
-    <p class="text-sm px-1">{{project.description}}</p>
-    <p class="text-sm">{{project.keywords.join(', ')}}</p>
+    <p class="text-sm px-1">{{ project.description }}</p>
+    <p class="text-sm">{{ project.keywords.join(', ') }}</p>
   </div>
-  
+
 </template>
 
 <script setup>
