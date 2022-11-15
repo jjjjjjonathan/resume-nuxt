37261560b344bce21e273c0ab9b4ef5197f4bc46
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-11-15 00:11:36 -0500

    Change header shade of green

--- components/Basics/index.vue
+++ components/Basics/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <header id="basics" class="flex flex-col bg-emerald-300 w-full p-4">
+  <header id="basics" class="flex flex-col bg-emerald-400 w-full p-4">
     <div class="flex flex-row justify-between items-end">
       <h1 class="text-3xl font-semibold">{{ name.toUpperCase() }}</h1>
       <h2 class="text-2xl font-semibold">{{ label.toUpperCase() }}</h2>
