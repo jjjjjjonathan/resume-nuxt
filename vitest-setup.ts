4074baf1f37c969a61307f4ddf84de679248242b
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-11-15 15:49:37 -0500

    Setup vitest and vitest-dom

---
+++ vitest-setup.ts
@@ -0,0 +1,11 @@
+import * as matchers from 'vitest-dom/matchers';
+import { expect } from 'vitest';
+import 'vitest-dom/extend-expect';
+
+expect.extend(matchers);
+
+export default defineAppConfig({
+  test: {
+    setupFiles: ['vitest-setup.ts'],
+  },
+});
