c40d759652cf7962e44e1894be4a032088f12af4
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-11-16 11:00:01 -0500

    Make content responsive to screen sizes

--- components/Basics/Summary.vue
+++ components/Basics/Summary.vue
@@ -1,12 +1,12 @@
 <template>
-  <section class="flex flex-col px-4">
+  <section class="flex flex-col px-4 col-span-11 order-0">
     <ContentHeading :title="SUMMARY" :full="true" />
-    <p class="text-sm px-4 py-2">{{summary}}</p>
+    <p class="text-sm px-4 py-2">{{ summary }}</p>
   </section>
 </template>
 
 <script setup>
-  import { getBasics } from '../Basics/helpers';
-  const SUMMARY = 'summary'
-  const summary = getBasics(SUMMARY)
+import { getBasics } from '../Basics/helpers';
+const SUMMARY = 'summary';
+const summary = getBasics(SUMMARY);
 </script>
--- components/Content/index.vue
+++ components/Content/index.vue
@@ -1,24 +1,26 @@
 <template>
-  <div class="grid grid-cols-11 justify-center w-full items-center">
-    <div class="col-span-6 h-full flex flex-col">
-      <section class="px-4">
-        <ContentHeading :title="'projects'" />
-        <Projects />
-      </section>
-      <section class="px-4">
-        <ContentHeading :title="'work'" />
-        <Work />
-      </section>
-    </div>
-    <div class="col-span-5 h-full flex flex-col">
-      <section class="px-4">
-        <ContentHeading :title="'skills'" />
-        <Skills />
-      </section>
-      <section class="px-4">
-        <ContentHeading :title="'education'" />
-        <Education />
-      </section>
-    </div>
+  <div class="grid grid-cols-11 justify-center w-full items-center mb-5 letter:mb-0">
+    <BasicsSummary />
+
+    <section class="px-4 col-span-11 small:col-span-6 order-2 small:order-1">
+      <ContentHeading :title="'projects'" />
+      <Projects />
+    </section>
+
+    <section class="px-4 col-span-11 small:col-span-5 order-1 small:order-2">
+      <ContentHeading :title="'skills'" />
+      <Skills />
+    </section>
+
+    <section class="px-4 col-span-11 small:col-span-6 order-3">
+      <ContentHeading :title="'work'" />
+      <Work />
+    </section>
+
+    <section class="px-4 col-span-11 small:col-span-5 order-4">
+      <ContentHeading :title="'education'" />
+      <Education />
+    </section>
+
   </div>
 </template>
