4f33c8b443206a84345e16d53acfa3eb01659d6d
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-11-16 11:06:25 -0500

    Switch to one-column layout at medium breakpoint

--- components/Content/index.vue
+++ components/Content/index.vue
@@ -1,23 +1,23 @@
 <template>
-  <div class="grid grid-cols-11 justify-center w-full items-center mb-5 letter:mb-0">
+  <div class="grid grid-cols-11 justify-center w-full h-full items-center mb-5 letter:mb-0">
     <BasicsSummary />
 
-    <section class="px-4 col-span-11 small:col-span-6 order-2 small:order-1">
+    <section class="px-4 col-span-11 medium:col-span-6 order-2 medium:order-1">
       <ContentHeading :title="'projects'" />
       <Projects />
     </section>
 
-    <section class="px-4 col-span-11 small:col-span-5 order-1 small:order-2">
+    <section class="px-4 col-span-11 medium:col-span-5 order-1 medium:order-2">
       <ContentHeading :title="'skills'" />
       <Skills />
     </section>
 
-    <section class="px-4 col-span-11 small:col-span-6 order-3">
+    <section class="px-4 col-span-11 medium:col-span-6 order-3">
       <ContentHeading :title="'work'" />
       <Work />
     </section>
 
-    <section class="px-4 col-span-11 small:col-span-5 order-4">
+    <section class="px-4 col-span-11 medium:col-span-5 order-4">
       <ContentHeading :title="'education'" />
       <Education />
     </section>
