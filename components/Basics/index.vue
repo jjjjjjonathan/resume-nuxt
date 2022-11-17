fb7c4f1e40e5b36f1fdfce6244a3931656555148
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-11-16 23:23:54 -0500

    Lower font size of header

--- components/Basics/index.vue
+++ components/Basics/index.vue
@@ -2,8 +2,8 @@
   <header id="basics" class="flex flex-col small:flex-row medium:flex-col bg-emerald-400 w-full p-4 justify-between">
     <div
       class="flex flex-col medium:flex-row justify-between items-center small:items-start medium:items-end print:flex-row print:items-end">
-      <h1 class="text-5xl font-semibold text-center xs:text-right">{{ name.toUpperCase() }}</h1>
-      <h2 class="text-3xl font-semibold text-center xs:text-right">{{ label.toUpperCase() }}</h2>
+      <h1 class="text-4xl font-semibold text-center xs:text-right">{{ name.toUpperCase() }}</h1>
+      <h2 class="text-2xl font-semibold text-center xs:text-right">{{ label.toUpperCase() }}</h2>
     </div>
     <BasicsLinks />
   </header>
