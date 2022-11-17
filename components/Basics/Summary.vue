66c191734c7af383b3014489c834011463a57adb
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-11-17 15:23:24 -0500

    Add rubik font to css

--- components/Basics/Summary.vue
+++ components/Basics/Summary.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="flex flex-col justify-start px-4 col-span-11 order-0 h-full letter:-mb-3">
+  <section class="flex flex-col justify-start px-4 col-span-11 order-0 h-full">
     <ContentHeading :title="SUMMARY" />
     <p class="text-sm px-4 pt-2">{{ summary }}</p>
   </section>
