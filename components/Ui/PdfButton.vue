c6260686dad70126b96c3d7ceb28c203445f20e6
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-11-22 19:46:39 -0500

    Remove button and style anchor as button

--- components/Ui/PdfButton.vue
+++ components/Ui/PdfButton.vue
@@ -1,10 +1,9 @@
 <template>
-  <a href="https://resume.jcheng.ca/Cheng_Jonathan-resume.pdf">
-    <button
-      class="my-auto rounded-lg bg-emerald-200 hover:bg-emerald-300 shadow-md p-1 block print:hidden"
-    >
-      <span class="inline medium:hidden large:inline">View as </span>
-      <Icon name="fa6-regular:file-pdf" /> PDF
-    </button>
+  <a
+    href="https://resume.jcheng.ca/Cheng_Jonathan-resume.pdf"
+    class="my-auto rounded-lg bg-emerald-200 hover:bg-emerald-300 shadow-md p-1 block print:hidden"
+  >
+    <span class="inline medium:hidden large:inline">View as </span>
+    <Icon name="fa6-regular:file-pdf" /> PDF
   </a>
 </template>
